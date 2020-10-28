0db085a4516c5a34b48c270790928d797ef6d691
aNoma-ly <44550798+aNoma-ly@users.noreply.github.com>
2020-10-28 20:31:14 +0000

    Add files via upload

---
+++ include/header.php
@@ -0,0 +1,20 @@
+<?php include("include/config.php");
+include("include/classes/Artist.php"); 
+include("include/classes/Album.php"); 
+include("include/classes/Song.php"); 
+?>
+<html>
+<head>
+	<title>Chime™</title>
+
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link rel="icon" href="assets/images/ icons/chimelogo.png">
+</head>
+
+<body>
+
+	<div id="container">
+
+	<?php include("include/navContainer.php"); ?>
+	<div id="mainView">
+		<div id="mainContent">
---
+++ include/playbackBarContainer.php
@@ -0,0 +1,97 @@
+<div id="playbackBarContainer">
+
+			<div id="playbackBar">
+
+				<div id="playbackBarLeft">
+					<div class="bar">
+						<span class="album">
+							<img src="" class="artwork">
+						</span>
+
+						<div class="songInfo">
+								
+							<span class="songName">
+								<span></span>
+							</span>
+
+							<span class="authorName">
+								<span></span>
+							</span>
+							
+						</div>
+
+					</div>
+
+				</div>
+
+				<div id="playbackBarCenter">
+
+					<div class="controls bar">
+						
+						<div class="button">
+
+							<button class="butControl shuffle" title="Shuffle button">
+								<img src="assets/images/icons/shuffle.png" alt="Shuffle">
+							</button>
+
+							<button class="butControl previous" title="Previous button">
+								<img src="assets/images/icons/previous.png" alt="Previous">
+							</button>
+
+							<button class="butControl play" title="Play button">
+								<img src="assets/images/icons/play.png" alt="Play">
+							</button>
+
+							<button class="butControl pause" title="Pause button" style="display: none;">
+								<img src="assets/images/icons/pause.png" alt="Pause">
+							</button>
+
+							<button class="butControl next" title="Next button">
+								<img src="assets/images/icons/next.png" alt="Next">
+							</button>
+
+							<button class="butControl repeat" title="Repeat button">
+								<img src="assets/images/icons/repeat.png" alt="Repeat">
+							</button>
+
+						</div>
+
+
+						<div class="playBar">
+							
+							<span class="time current">0.00</span>
+
+							<div class="progressBar">
+								<div class="progressBarBG">
+									<div class="progress"></div>
+								</div>
+							</div>
+
+							<span class="time remaining">0.00</span>
+
+
+						</div>
+
+
+					</div>
+
+				
+				</div>
+
+				<div id="playbackBarRight">
+					<div class="volumeBar">
+
+						<button class="butControl volume" title="Volume button">
+							<img src="assets/images/icons/volume.png" alt="Volume">
+						</button>
+
+						<div class="progressBar">
+								<div class="progressBarBG">
+									<div class="progress"></div>
+								</div>
+						</div>
+
+					</div>
+				</div>
+
+			</div>
